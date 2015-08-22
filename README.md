0098eec16cc80b772961599af0d194b98c9ea01a
Lee Fallat <me@leefallat.ca>
2015-08-22 04:06:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# snippet - A cryptographically verifyable pastebin service.
+snippet is a self-contained PHP action page that takes a user's content and creates a SHA256 sum from it. This SHA256 sum is used to name the file that will store the content, and is used as a reference in the URL.
---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype HTML>
+<head>
+	<title>Paste</title>
+	<link rel="stylesheet" type="text/css" href="./paste.css"/>
+</head>
+<body>
+<?php
+	if(isset($_GET['hash'])) {
+		$text = htmlspecialchars(file_get_contents($_GET['hash'].".txt"));
+		?>
+
+		<form action="/paste/" method="GET" class="action">
+			<input type="submit" value="New"/>
+		</form>
+
+		<?php
+		echo "<pre>{$text}</pre>";
+
+	} else if(isset($_POST['file'])) {
+		$hash = hash("sha256", $_POST['file']);
+		file_put_contents("{$hash}.txt", $_POST['file']);
+		header("Location:/paste/?hash={$hash}");
+
+	} else {
+		?>
+
+		<form action="/paste/" method="POST">
+			<textarea rows="40" cols="80" name="file"></textarea>
+			<input type="submit" class="top action" value="Paste"/>
+		</form>
+
+		<?php
+
+	}
+
+?>
+</body>
+</html>
